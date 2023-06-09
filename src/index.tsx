7b7e05abf79b46732c371bbc396139782646afca
MindHunter <62446086+Beqa-Beqa@users.noreply.github.com>
2023-06-09 12:03:36 +0400

    Update index.tsx to hash router

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
